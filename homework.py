8546ff4a137498d97717506afba29892c940032e
UfoNexus <evans.d@yandex.ru>
2021-06-21 19:17:54 +0300

    started work on recording

--- homework.py
+++ homework.py
@@ -2,8 +2,26 @@ import datetime as dt
 
 
 class Calculator:
-    pass
+    def __init__(self, limit):
+        self.limit = limit
+
+    records = []
+
+    def add_record(amount, date, comment):
+        i = len(records)
+        records.append(Record(amount, date, comment))
 
 
 class Record:
+    def __init__(self, amount, date, comment):
+        self.amount = amount
+        self.date = date
+        self.comment = comment
+
+
+class CaloriesCalculator(Calculator):
+    pass
+
+
+class CashCalculator(Calculator):
     pass
